6acc8894ffb7126f6822894bcc33b970d7685f69
Andrew Hynes <andrew.hynes@hotmail.co.uk>
2014-05-08 00:10:40 +0100

    Added package related things for Marmalade

--- eprime-mode.el
+++ eprime-mode.el
@@ -1,7 +1,40 @@
-;;;;eprime-mode.el - An E-prime mode for Emacs, which warns users when they 
-;;;;write something that does not conform to E-prime.
-;;;;Read more here - https://en.wikipedia.org/wiki/E-prime
-;;;;Naturally, all of this file that can do, conforms to E'
+;;; eprime-mode.el --- An E-prime checking mode for Emacs
+
+;; Copyright 2014 Andrew Hynes
+
+;; Filename: eprime-mode.el
+;; Author: Andrew Hynes <andrewhynes@openmailbox.org>
+;; URL: https://github.com/AndrewHynes/eprime-mode
+;; Description: An E-prime checking mode for Emacs that highlights non-conforming text.
+;; Version: 1.0
+;; Keywords: E-prime English
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;; 
+;; * About eprime-mode
+;; An E-prime checking mode for Emacs. 
+;; Read more here - https://en.wikipedia.org/wiki/E-prime
+;; Naturally, all of this file that can, conforms to E'
+;;
+;; Adds the following functionality: 
+;;
+;;   - M-x eprime-check-buffer to check the buffer
+;; 
+;;   - A minor mode, eprime-mode, which checks the buffer and
+;;     any text you enter thereafter.
+;; 
+;;   - M-x eprime-remove corrections to remove its corrections
+;; 
+;;   - Customisable face for banned words. eprime-banned-words-face
+;; 
+;;   - M-x eprime-check-word to check only the current word
+;;
+;;   - Can customise banned words (by pushing onto eprime-baned-words)
+;;
+;;   - Default different face than FlySpell for ease of use together
+;; 
+
 
 ;; Try not to hate me -
 (require 'cl)
@@ -115,4 +148,6 @@
 ;;AM BE BEING AM IS WHAT'S
 
 (provide 'eprime-mode)
+
+;;; eprime-mode.el ends here
 
