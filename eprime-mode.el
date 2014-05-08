7b25c459572459f9e019897c2ad5684fb2d912ca
Andrew Hynes <andrew.hynes@hotmail.co.uk>
2014-05-08 01:10:15 +0100

    Mentioned GPLv3

--- eprime-mode.el
+++ eprime-mode.el
@@ -1,13 +1,27 @@
 ;;; eprime-mode.el --- An E-prime checking mode for Emacs
 
-;; Copyright 2014 Andrew Hynes
+;; Copyright (C) 2014 Andrew Hynes
 
 ;; Filename: eprime-mode.el
 ;; Author: Andrew Hynes <andrewhynes@openmailbox.org>
 ;; URL: https://github.com/AndrewHynes/eprime-mode
 ;; Description: An E-prime checking mode for Emacs that highlights non-conforming text.
 ;; Version: 1.0
-;; Keywords: E-prime English
+;; Keywords: E-prime, English, grammar
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Commentary:
